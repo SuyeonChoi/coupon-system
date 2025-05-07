4caae14d6045e49dca068824cd27549bd2962b3b
SuyeonChoi <tndus970120@naver.com>
2025-05-07 12:06:04 +0900

    test: 멀티스레드 테스트 케이스 추가

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
@@ -23,5 +28,33 @@ class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000; // 요청 수
+
+        // 동시에 여러개 요청을 보내기 위해 멀티 스레드 사용
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 모든 요청이 끝날때까지 기다림
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // 모든 요청 완료 시 생성된 쿠폰 개수 확인
+        long count = couponRepository.count();
+        assertThat(count).isEqualTo(100);
     }
 }
