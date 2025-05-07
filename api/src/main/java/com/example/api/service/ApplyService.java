453c9c10104ecbc421a132179c46f3ea762b834e
SuyeonChoi <tndus970120@naver.com>
2025-05-07 10:24:31 +0900

    feat: 기본 쿠폰 발급 로직 작성

---
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -0,0 +1,26 @@
+package com.example.api.service;
+
+import com.example.api.domain.Coupon;
+import com.example.api.repository.CouponRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplyService {
+
+    private final CouponRepository couponRepository;
+
+    public ApplyService(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
+    }
+
+    public void apply(Long userId) {
+        long count = couponRepository.count();
+
+        if (count > 100) {
+            return; // 발급하지 않음
+        }
+
+        // 발급 가능한 경우 쿠폰 생성
+        couponRepository.save(new Coupon(userId));
+    }
+}
