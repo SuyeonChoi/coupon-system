5b897e0285a6b0ac9bcba2ec426afee58eecb191
SuyeonChoi <tndus970120@naver.com>
2025-05-13 02:14:10 +0900

    feat: 쿠폰 생성 중 에러 발생 시 백업 데이터 저장 및 로그 추가

---
+++ consumer/src/main/java/com/example/consumer/consumer/repository/FailedEventRepository.java
@@ -0,0 +1,8 @@
+package com.example.consumer.consumer.repository;
+
+
+import com.example.consumer.consumer.domain.FailedEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FailedEventRepository extends JpaRepository<FailedEvent, Long> {
+}
